f737112e69f12744d111346d5c921707779b8898
Manarjan <manarjan.s.ghai@gmail.com>
2023-04-21 20:27:05 +0530

    scaffold refactor for counselling

--- apps/sjfl-judians/src/app/features/App/Counselling/pages/CounsellingLandingPage.tsx
+++ apps/sjfl-judians/src/app/features/App/Counselling/pages/CounsellingLandingPage.tsx
@@ -1,31 +1,31 @@
-import Toolbar from '@mui/material/Toolbar';
-import AppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { t, Trans } from '@lingui/macro';
-import { NavLink, Outlet } from 'react-router-dom';
+import { t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import IconButton from '@mui/material/IconButton';
+import { NavLink, Outlet } from 'react-router-dom';
 
-import { Page } from '../../../../shared/components/containers/Page';
-import { FilterChip } from '../../../../shared/components/containers/FilterChip';
 import { AppHeader } from '../../../../shared/components/containers/AppHeader';
+import { FilterChip } from '../../../../shared/components/containers/FilterChip';
+import { Scaffold } from '../../../../shared/components/containers/Scaffold';
 
 export const CounsellingLandingPage = () => {
   const { i18n } = useLingui();
+  const header = (
+    <AppHeader
+      slots={{
+        left: <CalendarMonthIcon color="primary" />,
+        right: (
+          <IconButton size="large" edge="end">
+            <AddCircleOutlineIcon />
+          </IconButton>
+        ),
+      }}
+      title={i18n._(t({ id: 'Counselling.Header', message: 'Counselling' }))}
+    />
+  );
   return (
-    <Page className="flex flex-col">
-      <AppHeader
-        slots={{
-          left: <CalendarMonthIcon color="primary" />,
-          right: (
-            <IconButton size="large" edge="end">
-              <AddCircleOutlineIcon />
-            </IconButton>
-          ),
-        }}
-        title={i18n._(t({ id: 'Counselling.Header', message: 'Counselling' }))}
-      />
+    <Scaffold header={header}>
       <div className="flex gap-4 p-4">
         <NavLink to="upcoming" className="flex-grow" draggable={false}>
           {({ isActive }) => (
@@ -49,6 +49,6 @@ export const CounsellingLandingPage = () => {
         </NavLink>
       </div>
       <Outlet></Outlet>
-    </Page>
+    </Scaffold>
   );
 };
