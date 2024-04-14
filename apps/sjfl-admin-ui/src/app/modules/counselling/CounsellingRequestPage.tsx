92bdbeab16b452c6b5102c1bccf836e6f6b92576
Sreelekshmy-S <sreelekshmi5149@gmail.com>
2024-04-14 17:39:28 +0530

    Counselling Request

--- apps/sjfl-admin-ui/src/app/modules/counselling/CounsellingRequestPage.tsx
+++ apps/sjfl-admin-ui/src/app/modules/counselling/CounsellingRequestPage.tsx
@@ -22,14 +22,16 @@ export const CounsellingRequestPage = () => {
         <div className="flex flex-col gap-4">
           <Card>
             <CardContent className="flex flex-col items-center justify-center gap-2">
+             <div className="w-32 h-32">
               <Avatar color="red">
-                <AvatarImage
-                  src={
-                    `https://robohash.org/${matchedJudian?.id}`
-                  }
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
+                  <AvatarImage
+                    src={
+                      `https://robohash.org/${matchedJudian?.id}`
+                    }
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+             </div>
               <Text as="h2" className="text-base">{matchedJudian?.name}</Text>
               <Text className="text-base">{matchedJudian?.phone}</Text>
               <Button type="button" variant={'default'}>
@@ -39,127 +41,103 @@ export const CounsellingRequestPage = () => {
           </Card>
           <Card>
             <CardContent className="flex flex-col gap-2">
-              <Text as="h2" className="text-base mb-3">Information</Text>
-              <Table>
-                <TableBody>
-                <TableRow>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3">Date Of Birth</Text>
-                  </TableCell>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3"></Text>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3">Gender</Text>
-                  </TableCell>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3"></Text>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3">UID</Text>
-                  </TableCell>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3"></Text>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3">Last Session</Text>
-                  </TableCell>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3"></Text>
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3">Judian Since</Text>
-                  </TableCell>
-                  <TableCell>
-                    <Text as="h2" className="text-base mb-3"></Text>
-                  </TableCell>
-                </TableRow>
-                </TableBody>
-              </Table>
+              <Text as="h2" className="text-base mb-3 font-bold">Information</Text>
+              <div  className="flex flex-col gap-2">
+                <div className="flex">
+                  <div>Date Of Birth</div>
+                  <div></div>
+                </div>
+                <div className="flex">
+                  <div>Gender</div>
+                  <div></div>
+                </div>
+                <div className="flex">
+                  <div>UID</div>
+                  <div></div>
+                </div>
+                <div className="flex">
+                  <div>Last Session</div>
+                  <div></div>
+                </div>
+                <div className="flex">
+                  <div>Judian Since</div>
+                  <div></div>
+                </div>
+              </div>
             </CardContent>
           </Card>
         </div>
         <div className="gap-4">
           <Card>
             <CardContent className="flex flex-col justify-start gap-2">
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Approved Date</TableHead>
-                    <TableHead>Approved Time</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>
-                      <Text as="h2" className="text-base mb-3"></Text>
-                    </TableCell>
-                    <TableCell>
-                      <Text as="h2" className="text-base mb-3"></Text>
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
+            <div  className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                  <div className="font-bold">Approved Date</div>
+                  <div className="font-bold">Approved Time</div>
+                </div>
+                <div className="flex">
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
               <div className="flex flex-col gap-2">
                 <Text as="h2" className="text-base">Notes</Text>
                 <Text className="text-base">Notes provided by Judian while creating the request.</Text>
               </div>
               <div className="flex gap-2">
                 <Text as="h2" className="text-base">Status</Text>
-                <Text className="text-base">Approved</Text>
+                <Text className="text-base text-xl text-green-600 font-bold">Approved</Text>
               </div>
               <div className="flex gap-2">
                 <Text className="text-base">Note for Judian</Text>
               </div>
               <div className="flex justify-center gap-4">
-                <Button type="button">Cancel</Button>
-                <Button type="submit">Reschedule</Button>
-                <Button type="submit">Complete</Button>
-          </div>
+                <Button type="button" variant="default">Cancel</Button>
+                <Button type="submit"  variant="destructive">Reschedule</Button>
+                <Button type="submit" variant="secondary">Complete</Button>
+              </div>
             </CardContent>
           </Card>
         </div>
         <div className="gap-4">
           <Card>
             <CardContent className="flex flex-col gap-2">
-              <Text as="h2" className="text-base">Previous Sessions</Text>
+              <Text as="h2" className="text-base font-bold">Previous Sessions</Text>
               <Card>
-                <CardContent className="flex flex-col items-center justify-center gap-2">
+                <CardContent className="flex flex-col justify-center gap-2">
                   <div className="flex gap-2">
                     <CalendarDays />
+                    <Text className="text-base">29/12/2023</Text>
                   </div>
                   <div className="flex gap-2">
                     <WatchIcon />
+                    <Text className="text-base">6:00 PM</Text>
                   </div>
                   <Text className="text-base">Session Note</Text>
                 </CardContent>
               </Card>
               <Card>
-                <CardContent className="flex flex-col items-center justify-center gap-2">
+                <CardContent className="flex flex-col justify-center gap-2">
                 <div className="flex gap-2">
                     <CalendarDays />
+                    <Text className="text-base">29/12/2023</Text>
                   </div>
                   <div className="flex gap-2">
                     <WatchIcon />
+                    <Text className="text-base">6:00 PM</Text>
                   </div>
                   <Text className="text-base">Session Note</Text>
                 </CardContent>
               </Card>
               <Card>
-                <CardContent className="flex flex-col items-center justify-center gap-2">
+                <CardContent className="flex flex-col justify-center gap-2">
                   <div className="flex gap-2">
                     <CalendarDays />
+                    <Text className="text-base">29/12/2023</Text>
                   </div>
                   <div className="flex gap-2">
                     <WatchIcon />
+                    <Text className="text-base">6:00 PM</Text>
                   </div>
                   <Text className="text-base">Session Note</Text>
                 </CardContent>
